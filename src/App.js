75e202a1c30069fb6c06f7701556375da19c44d9
Micweezy <michealakinse@yahoo.com>
2022-06-05 11:57:39 +0100

    Practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import profile from "./Image/b.png";
+import email from "./Image/email.jpg";
+import pass from "./Image/pass.png";
+import bank from "./Image/Green.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "main">
+      <div className = "brand-image">
+        <img src = {bank} alt = "bank" className = "bank"/>
+      </div>
+      <div className = "sub-main">
+        <div>
+          <div className = "imgs">
+            <div className = "container-image">
+              <img src = {profile} alt = "profile" className = "profile"/>
+            </div> 
+
+
+          </div>
+          <div>
+            <h1>Login Page</h1>
+            <div>
+              <img src = {email} alt = "email" className = "email"/>
+              <input type = "text" placeholder = "user name" className = "name"/>
+            </div>
+            <div className = "second-input">
+              <img src = {pass} alt = "pass" className = "email"/>
+              <input type = "text" placeholder = "password" className = "name"/>
+            </div>
+            <div className = "login-button">
+              <button>Login</button>
+            </div>
+            
+              <p className = "link">
+                <a href = "#">Forgot password ?</a> Or <a href = "#" >Sign Up</a> 
+              </p>
+            
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
